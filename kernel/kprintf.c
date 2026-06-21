112f21aab5081784ca24adebadceaa2b8d46a408
Lucas Gabriel <lucasilveira865@gmail.com>
2026-06-21 12:01:36 -0300

    Improving kprintf to support long values

--- kernel/kprintf.c
+++ kernel/kprintf.c
@@ -1,111 +1,154 @@
-#include <stdarg.h>
-#include "../drivers/uart.h"
 #include "kprintf.h"
 
-static inline void kprint_int(long long num){
-    char buffer[32];
-    unsigned int i = 0;
-    
-    if(num == 0) {
-        uart_putc('0');
-        return;
-    }
+#include <stdarg.h>
 
-    if(num < 0) {
-        uart_putc('-');
-        return;
-    }
+#include "../drivers/uart.h"
 
-    while(num > 0){
-        buffer[i++] = (num % 10) + '0';
-        num /= 10;
-        for(int j = i -1; j >= 0; j--){
-            uart_putc(buffer[j]);
-        }
-    }
+static void kprint_uint(unsigned long long value) {
+  char buffer[32];
+  int i = 0;
+
+  if (value == 0) {
+    uart_putc('0');
+    return;
+  }
+
+  while (value > 0) {
+    buffer[i++] = '0' + (value % 10);
+    value /= 10;
+  }
+
+  while (i--) {
+    uart_putc(buffer[i]);
+  }
 }
 
-static inline  void kprint_hex(unsigned long long num){
-    char buffer[32];
-    char hex_digits[] = "0123456789ABCDEF";
-    int i = 0;
+static void kprint_int(long long value) {
+  if (value < 0) {
+    uart_putc('-');
+    value = -value;
+  }
 
-    if (num == 0) {
-        uart_putc('0');
-        return;
-    }
-
-    while (num > 0) {
-        buffer[i++] = hex_digits[num % 16];
-        num /= 16;
-    }
-
-    for (int j = i - 1; j >= 0; j--) {
-        uart_putc(buffer[j]);
-    }
+  kprint_uint((unsigned long long)value);
 }
 
-void kprintf(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
+static void kprint_hex(unsigned long long value) {
+  char buffer[32];
+  const char* hex = "0123456789ABCDEF";
+  int i = 0;
 
-    for (int i = 0; fmt[i] != '\0'; i++) {
-        if (fmt[i] != '%') {
-            if (fmt[i] == '\n') {
-                uart_putc('\r'); 
-            }
-            uart_putc(fmt[i]);
-            continue;
-        }
+  if (value == 0) {
+    uart_putc('0');
+    return;
+  }
 
-        i++; 
+  while (value > 0) {
+    buffer[i++] = hex[value & 0xF];
+    value >>= 4;
+  }
 
-        switch (fmt[i]) {
-            case '%':
-                uart_putc('%');
-                break;
+  while (i--) {
+    uart_putc(buffer[i]);
+  }
+}
 
-            case 'c': { 
-                char c = (char)va_arg(args, int);
-                uart_putc(c);
-                break;
-            }
+static void kprint_string(const char* str) {
+  if (!str) {
+    str = "(null)";
+  }
 
-            case 's': { 
-                char *s = va_arg(args, char *);
-                if (!s) s = "(null)";
-                while (*s) {
-                    if (*s == '\n') uart_putc('\r');
-                    uart_putc(*s++);
-                }
-                break;
-            }
-
-            case 'd': { 
-                int d = va_arg(args, int);
-                kprint_int(d);
-                break;
-            }
-
-            case 'x': { 
-                unsigned int x = va_arg(args, unsigned int);
-                kprint_hex(x);
-                break;
-            }
-
-            case 'p': { 
-                unsigned long long p = va_arg(args, unsigned long long);
-                uart_puts("0x");
-                kprint_hex(p);
-                break;
-            }
-
-            default: 
-                uart_putc('%');
-                uart_putc(fmt[i]);
-                break;
-        }
+  while (*str) {
+    if (*str == '\n') {
+      uart_putc('\r');
     }
 
-    va_end(args);
+    uart_putc(*str++);
+  }
+}
+
+void kprintf(const char* fmt, ...) {
+  va_list args;
+
+  va_start(args, fmt);
+
+  while (*fmt) {
+    if (*fmt != '%') {
+      if (*fmt == '\n') {
+        uart_putc('\r');
+      }
+
+      uart_putc(*fmt++);
+      continue;
+    }
+
+    fmt++;
+
+    if (*fmt == 'l') {
+      fmt++;
+
+      switch (*fmt) {
+        case 'd':
+          kprint_int(va_arg(args, long));
+          break;
+
+        case 'u':
+          kprint_uint(va_arg(args, unsigned long));
+          break;
+
+        case 'x':
+          kprint_hex(va_arg(args, unsigned long));
+          break;
+
+        default:
+          uart_putc('%');
+          uart_putc('l');
+          uart_putc(*fmt);
+          break;
+      }
+
+      fmt++;
+      continue;
+    }
+
+    switch (*fmt) {
+      case '%':
+        uart_putc('%');
+        break;
+
+      case 'c':
+        uart_putc((char)va_arg(args, int));
+        break;
+
+      case 's':
+        kprint_string(va_arg(args, char*));
+        break;
+
+      case 'd':
+        kprint_int(va_arg(args, int));
+        break;
+
+      case 'u':
+        kprint_uint(va_arg(args, unsigned int));
+        break;
+
+      case 'x':
+        kprint_hex(va_arg(args, unsigned int));
+        break;
+
+      case 'p':
+        uart_puts("0x");
+
+        kprint_hex((unsigned long long)va_arg(args, void*));
+        break;
+
+      default:
+        uart_putc('%');
+        uart_putc(*fmt);
+        break;
+    }
+
+    fmt++;
+  }
+
+  va_end(args);
 }
